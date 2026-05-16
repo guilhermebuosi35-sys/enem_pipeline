49023c7d68bfc62d41e4e1c281c10a44a821d8f2
Macbook <macbook@Macbooks-MacBook-Pro.local>
2026-05-16 19:23:06 -0300

    build: Script de extração dos dados no site do governo

--- src/extract.py
+++ src/extract.py
@@ -1,1 +1,16 @@
-print ('teste')
+import requests
+from pathlib import Path
+import certifi
+
+links = ["https://download.inep.gov.br/microdados/microdados_enem_2024.zip", "https://download.inep.gov.br/microdados/microdados_enem_2023.zip","https://download.inep.gov.br/microdados/microdados_enem_2022.zip"]
+ 
+for arquivo in links: 
+    resposta = requests.get(arquivo, stream=True, verify="/Users/macbook/enem_pipeline/rnp_cert.pem")
+    nome = Path(arquivo).name
+    pasta = Path("/Users/macbook/enem_pipeline/data/raw")
+
+    pasta.mkdir(parents=True, exist_ok=True) 
+
+    with open (pasta / nome, "wb") as file:
+        for chunk in resposta.iter_content(chunk_size=8192):
+            file.write(chunk)
