483d4dda3932caed4a093903a5f095465348f21a
Guilherme Buosi <guilhermebuosi35@gmail.com>
2026-06-28 22:12:47 -0300

    feat: Gráfico com a distribuição de Notas por região

--- streamlit/pages/visao_geral.py
+++ streamlit/pages/visao_geral.py
@@ -3,6 +3,8 @@ import streamlit as st
 from dotenv import load_dotenv
 import plotly.express as px
 from mapeamentos import *
+import json
+from urllib.request import urlopen
 
 load_dotenv()
 
@@ -23,6 +25,13 @@ conn = st.connection("postgresql", type="sql", **secrets)
 st.markdown("## Distribuição de Renda Familiar")
 
 option_map = ["2022", "2023", "2024"]
+materias_dict = {
+    "nota_cn": "Ciências da Natureza",
+    "nota_ch": "Ciências Humanas",
+    "nota_lc": "Linguagens",
+    "nota_mt": "Matemática",
+    "nota_redacao": "Redação"
+}
 
 ano_renda = st.pills(
     "Ano de referência (Renda)",
@@ -94,13 +103,7 @@ if ano_media_nota:
             '''
         )
 
-        df_media_nota_rn = df_media_nota.rename(columns={
-            "nota_cn": "Ciências da Natureza",
-            "nota_ch": "Ciências Humanas",
-            "nota_lc": "Linguagens",
-            "nota_mt": "Matemática",
-            "nota_redacao": "Redação"
-        })
+        df_media_nota_rn = df_media_nota.rename(columns=materias_dict)
 
         fig_media_nota = px.box(
             df_media_nota_rn, 
@@ -114,3 +117,59 @@ if ano_media_nota:
         fig_media_nota.update_layout(showlegend=False)
 
     st.plotly_chart(fig_media_nota, use_container_width=True)
+
+# SESSÃO 2: Distribuição de Notas por Região
+st.markdown("## Distribuição de Notas por Região")
+
+ano_media_regiao = st.pills(
+    "Ano de referência (Região)",
+    options=option_map,
+    selection_mode="single",
+    default="2022",
+    key="selection_regiao"
+)
+
+materia_selecionada = st.pills(
+    "Matéria da Prova",
+    options=materias_dict.values(),
+    selection_mode='single',
+    default='Ciências da Natureza',
+    key='selection_materia'
+)
+
+if ano_media_regiao:
+
+    with urlopen("https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/brazil-states.geojson") as response:
+        brasil_geojson = json.load(response)
+
+    mat_query = [k for k,v in materias_dict.items() if v == materia_selecionada]
+
+    df_media_regiao = conn.query (f'''
+        SELECT 
+            uf_prova,
+            ROUND(AVG({mat_query[0]}), 2) AS media 
+        FROM silver.vw_resultados_{ano_media_regiao}                 
+        GROUP BY uf_prova 
+        ORDER BY media DESC
+    ''')
+
+    tab1, tab2 = st.tabs(["Gŕafico", "Tabela"])
+
+    fig = px.choropleth_map(
+        data_frame=df_media_regiao,
+        geojson=brasil_geojson,
+        locations='uf_prova',
+        featureidkey='properties.sigla',
+        center = {"lat": -14.235004, "lon": -51.92528},
+        color='media',
+        labels={'media': 'Média'},
+        zoom=3,
+        color_continuous_scale='blues' 
+    )
+
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+    with tab1:
+        st.plotly_chart(fig, use_container_width=True)
+    with tab2:
+        st.dataframe({'UF': df_media_regiao['uf_prova'], 'Média': df_media_regiao['media']})
